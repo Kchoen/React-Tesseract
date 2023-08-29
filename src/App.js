8492d0b3db5086a4bc0b190135dbec0e18b72995
Kchoen <kchoena@gmail.com>
2023-08-29 10:14:43 +0800

    change page to restaurant picker

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import FileUpload from "./components/FileUpload";
-
+import RestaurantPicker from "./components/Picker";
 function App() {
 	return (
 		<div className="container mt-4">
-			<h4 className="display-4 text-center mb-4">
-				<i className="fab fa-react" /> React File Upload
-			</h4>
-
-			<FileUpload />
+			<RestaurantPicker />
 		</div>
 	);
 }
---
+++ src/components/Picker.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import { Button, Form, ListGroup } from "react-bootstrap";
+
+const RestaurantPicker = () => {
+	//...component code
+	const [selectedRestaurant, setSelectedRestaurant] = useState("selected");
+	const [restaurants, setRestaurants] = useState([]);
+	// Load initial state
+	React.useEffect(() => {
+		loadRestaurants();
+	}, []);
+
+	React.useEffect(() => {
+		saveRestaurants();
+	}, [restaurants]);
+
+	function loadRestaurants() {
+		const storedRestaurants = JSON.parse(
+			localStorage.getItem("restaurants")
+		);
+		setRestaurants(storedRestaurants);
+	}
+
+	function saveRestaurants() {
+		if (restaurants?.length)
+			localStorage.setItem("restaurants", JSON.stringify(restaurants));
+	}
+
+	const handleAddRestaurant = (e) => {
+		e.preventDefault();
+		const form = e.target;
+		const input = form.restaurant;
+
+		if (input.value.trim() === "") {
+			return;
+		}
+		const newRestaurant = e.target.restaurant.value;
+		setRestaurants((prevRestaurants) => {
+			return [...prevRestaurants, newRestaurant];
+		});
+	};
+	const handlePickRestaurant = () => {
+		const random = Math.floor(Math.random() * restaurants.length);
+		setSelectedRestaurant(restaurants[random]);
+	};
+	const handleDeleteRestaurant = (restaurant) => {
+		setRestaurants((prev) => prev.filter((r) => r !== restaurant));
+	};
+	return (
+		<div className="container mt-5">
+			<h1>口袋餐廳名冊</h1>
+
+			<Form onSubmit={handleAddRestaurant}>
+				<Form.Control
+					name="restaurant"
+					placeholder="Add a restaurant"
+				/>
+				<Button variant="primary" type="submit">
+					增加餐廳
+				</Button>
+			</Form>
+
+			<Button variant="success" onClick={handlePickRestaurant}>
+				挑選隨機餐廳
+			</Button>
+
+			<div className="mt-3">
+				<strong>挑中:</strong> {selectedRestaurant}
+			</div>
+
+			<ListGroup>
+				{restaurants.map((r) => (
+					<ListGroup.Item
+						key={r}
+						style={{
+							display: "flex",
+							alignItems: "center",
+							justifyContent: "center",
+						}}
+					>
+						{r + " "}
+
+						<Button
+							style={{
+								verticalAlign: "middle",
+								marginLeft: "auto",
+								float: "right",
+							}}
+							onClick={() => handleDeleteRestaurant(r)}
+							variant="danger"
+						>
+							刪除
+						</Button>
+					</ListGroup.Item>
+				))}
+			</ListGroup>
+		</div>
+	);
+};
+
+export default RestaurantPicker;
